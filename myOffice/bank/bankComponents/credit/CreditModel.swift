28516a1a9be46d675a1f228756de0d84a50eb42c
GrimProg <vavilovila01@gmail.com>
2026-05-02 20:51:10 +0600

    Сделал метод которые будет считать кредит

--- myOffice/bank/bankComponents/credit/CreditModel.swift
+++ myOffice/bank/bankComponents/credit/CreditModel.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+protocol CreditModel {
+    func perMounth(credit sum: Double, persent perMonth: Double, loan term: Int) -> Double
+}
--- myOffice/bank/bankComponents/credit/CreditViewModel.swift
+++ myOffice/bank/bankComponents/credit/CreditViewModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct CreditViewModel: CreditModel {
+    func perMounth(
+        credit sum: Double,
+        persent perMounth: Double,
+        loan term: Int
+    ) -> Double {
+        
+        let rate = perMounth
+        let n = Double(term)
+        
+        let powValue = pow(1 + rate, n)
+        
+        return sum * (rate * powValue) / (powValue - 1)
+    }
+}
