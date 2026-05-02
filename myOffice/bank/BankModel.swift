96667bc1d3aee81781723e877f7db0d7280de72f
GrimProg <vavilovila01@gmail.com>
2026-05-02 19:09:39 +0600

    добавил в проект раздел своего банка

---
+++ myOffice/bank/BankModel.swift
@@ -0,0 +1,8 @@
+//
+//  BankModel.swift
+//  myOffice
+//
+//  Created by Вавилов Илья on 2/5/26.
+//
+
+import Foundation
---
+++ myOffice/bank/BankView.swift
@@ -0,0 +1,18 @@
+//
+//  BankView.swift
+//  myOffice
+//
+//  Created by Вавилов Илья on 2/5/26.
+//
+
+import SwiftUI
+
+struct BankView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BankView()
+}
