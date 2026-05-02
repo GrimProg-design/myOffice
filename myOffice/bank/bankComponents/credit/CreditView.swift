b4612c78b1bf14adb66b7c2d6bd9c45c728dcd1a
GrimProg <vavilovila01@gmail.com>
2026-05-02 20:26:26 +0600

    Добавил пояснительные комментарии

---
+++ myOffice/bank/bankComponents/credit/CreditView.swift
@@ -0,0 +1,18 @@
+//
+//  CreditView.swift
+//  myOffice
+//
+//  Created by Вавилов Илья on 2/5/26.
+//
+
+import SwiftUI
+
+struct CreditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreditView()
+}
