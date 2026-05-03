69b83d74cdef9fda5b929a9fbf0b4c4727d4d5fe
GrimProg <vavilovila01@gmail.com>
2026-05-03 19:46:20 +0600

    Добавил метод для добавления в базу данных (SwiftData)

--- myOffice/bank/bankComponents/borrowers/BorrowersViewModel.swift
+++ myOffice/bank/bankComponents/borrowers/BorrowersViewModel.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import SwiftData
+
+@Observable
+final class BorrowersViewModel {
+    private let context: ModelContext
+    
+    init(context: ModelContext) {
+        self.context = context
+    }
+    
+    func saveBorrower(
+        person: String,
+        returnDate: String,
+        when: Date,
+        loan: Double,
+        returnSum: Double,
+        perMonth: Double,
+        months: [String]
+    ) {
+        let person = BorrowersModel(
+            name: person,
+            date: returnDate,
+            start: when,
+            summ: loan,
+            ret: returnSum,
+            perMonth: perMonth,
+            months: months
+        )
+        
+        context.insert(person)
+        
+        do {
+            try context.save()
+            print("Сохранение прошло успешно")
+        } catch {
+            print(error)
+        }
+    }
+}
